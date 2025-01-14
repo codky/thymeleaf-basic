8b562cc11ab75b5380bcbe1455e3e57660fd2bee
minseok kyeon <msky0912@gmail.com>
2025-01-14 10:42:36 +0900

    thymeleaf URL 링크

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -66,6 +66,13 @@ public class BasicController {
         }
     }
 
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
     @Data
     static class User {
         private String username;
